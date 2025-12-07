e6a58c0f44c35d8ea63caa9357e7e25d3dd964cc
JjuanGarci77 <JJuangarciaprograma123@gmail.com>
2025-12-07 13:24:35 -0500

    Creacion de menu de icon en index.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // Carrucel
 
     const track = document.querySelector(".carrusel-inner");
     const slides = track.querySelectorAll("img");
@@ -19,4 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(next, 3000);
     // PAra que oueda moverse y comenzar
     move();
+});
+
+const userIcon = document.querySelector(".user-icon"); //Icon menu despegable
+const userDropdown = document.querySelector(".user-dropdown");
+
+userIcon.addEventListener("click", () => {
+    // Muestra el menu con el clcick
+    userDropdown.style.display =
+        userDropdown.style.display === "flex" ? "none" : "flex";
+});
+
+// Cierra el menu con el clcik
+document.addEventListener("click", (e) => {
+    if (!e.target.closest(".user-menu-container")) {
+        userDropdown.style.display = "none";
+    }
 });
